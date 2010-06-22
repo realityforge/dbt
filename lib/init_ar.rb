e2c9945ab2dc9de0ad065d56f6a9d3df8e7df462
Peter Donald <peter@realityforge.org>
2010-06-22 15:00:11 +1000

    Remove linkage of plugin to common_tasks plugin

--- lib/init_ar.rb
+++ lib/init_ar.rb
@@ -8,25 +8,6 @@ unless Object.const_defined? :ActiveRecord
     end
   end
 
-  INCLUDED_PLUGIN_SET = ['common_tasks']
-
-  def add_to_deps(root_dir)
-    Dir.glob( File.join(root_dir, "**/lib" )).each do|dir|
-      if INCLUDED_PLUGIN_SET.include? File.basename(File.expand_path(dir + '/..' ))
-        ActiveSupport::Dependencies::load_paths << dir
-      end
-    end
-  end
-
-  def run_inits(root_dir)
-    Dir.glob( File.join(root_dir, "*" )).each do |dir|
-      if INCLUDED_PLUGIN_SET.include? File.basename(dir)
-        plugin_init_file = File.join(dir, 'init.rb')
-        require plugin_init_file if File.exists?( plugin_init_file )
-      end
-    end
-  end
-
   rails_root = defined?(::RAILS_FRAMEWORK_ROOT) ? ::RAILS_FRAMEWORK_ROOT : "#{RAILS_ROOT}/vendor/rails"
   require "#{rails_root}/activerecord/lib/activerecord.rb"
 
@@ -36,10 +17,5 @@ unless Object.const_defined? :ActiveRecord
 
   add_to_paths(gem_root)
   add_to_paths(plugin_root)
-  add_to_deps(plugin_root)
   add_to_paths(shared_plugin_root)
-  add_to_deps(shared_plugin_root)
-  run_inits(plugin_root)
-  run_inits(shared_plugin_root)
-
 end
