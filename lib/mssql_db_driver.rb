bf0913e340a050a4ab26d91f4212605e1a7669cf
Peter Donald <peter@realityforge.org>
2011-06-28 13:23:03 +1000

    Start to split out active record specific guff from config

--- lib/mssql_db_driver.rb
+++ lib/mssql_db_driver.rb
@@ -1,5 +1,5 @@
 class DbTasks
-  class MssqlDbConfig < DbTasks::DbConfig
+  class ActiveRecordDbConfig < DbTasks::DbConfig
     def initialize(configuration)
       @configuration = configuration
     end
@@ -9,6 +9,21 @@ class DbTasks
     def no_create?
       true == config_value("no_create", true)
     end
+
+    def catalog_name
+      config_value("database", false)
+    end
+
+    protected
+
+    def config_value(config_param_name, allow_nil)
+      value = self.configuration[config_param_name]
+      raise "Unable to locate configuration value named #{config_param_name}" if !allow_nil && value.nil?
+      value
+    end
+  end
+
+  class MssqlDbConfig < ActiveRecordDbConfig
 
     def force_drop?
       true == config_value("force_drop", true)
@@ -28,16 +43,6 @@ class DbTasks
 
     def instance_registry_key
       config_value("instance_registry_key", false)
-    end
-
-    def catalog_name
-      config_value("database", false)
-    end
-
-    def config_value(config_param_name, allow_nil)
-      value = self.configuration[config_param_name]
-      raise "Unable to locate configuration value named #{config_param_name}" if !allow_nil && value.nil?
-      value
     end
   end
 
