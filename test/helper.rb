22a417d7f8ea4e7a858ad2f2ddbdb46c9dd70a00
Peter Donald <peter@realityforge.org>
2013-05-31 18:15:09 +1000

    Reset more state in test

--- test/helper.rb
+++ test/helper.rb
@@ -14,6 +14,23 @@ class Dbt::TestCase < Minitest::Test
     Dbt::Config.default_search_dirs = nil
     Dbt::Config.default_no_create = nil
     Dbt::Config.config_filename = nil
+    Dbt::Config.datasets_dir_name = nil
+    Dbt::Config.repository_config_file = nil
+    Dbt::Config.default_up_dirs = nil
+    Dbt::Config.default_down_dirs = nil
+    Dbt::Config.default_finalize_dirs = nil
+    Dbt::Config.default_pre_create_dirs = nil
+    Dbt::Config.default_post_create_dirs = nil
+    Dbt::Config.default_pre_import_dirs = nil
+    Dbt::Config.default_post_import_dirs = nil
+    Dbt::Config.index_file_name = nil
+    Dbt::Config.default_import = nil
+    Dbt::Config.fixture_dir_name = nil
+    Dbt::Config.environment = nil
+    Dbt::Config.driver = nil
+    Dbt::Config.default_migrations_dir_name = nil
+    Dbt::Config.default_database = nil
+    Dbt::Config.task_prefix = nil
   end
 
   def teardown
