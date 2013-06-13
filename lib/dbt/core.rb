d7438d46d9be6aa8cc665136e0612572159d0dd7
Peter Donald <peter@realityforge.org>
2013-06-13 11:19:13 +1000

    Ensure some default values are set

--- lib/dbt/core.rb
+++ lib/dbt/core.rb
@@ -227,7 +227,7 @@ SQL
         @up_dirs = @down_dirs = @finalize_dirs = @pre_create_dirs = @post_create_dirs =
           @search_dirs = @migrations_dir_name = @migrations_applied_at_create =
             @rake_integration = @separate_import_task = @import_task_as_part_of_create =
-              @schema_overrides = nil
+              @schema_overrides = @datasets_dir_name = @fixture_dir_name = nil
 
       raise "schema_overrides should be derived from repository.yml and not directly specified." if options[:schema_overrides]
       raise "modules should be derived from repository.yml and not directly specified." if options[:modules]
