b5c8463ab418aa1d520f27e5696e24b2496d4cd8
Peter Donald <peter@realityforge.org>
2013-06-14 18:41:42 +1000

    Test dumping with data set specified

--- test/test_runtime_basic.rb
+++ test/test_runtime_basic.rb
@@ -781,6 +781,32 @@ class TestRuntimeBasic < Dbt::TestCase
     end
   end
 
+  def test_dump_database_to_fixtures_with_data_set
+    mock = Dbt::DbDriver.new
+    Dbt.runtime.instance_variable_set("@db", mock)
+
+    fixture_dir = create_dir("output_fixtures")
+
+    config = create_postgres_config()
+
+    db_scripts = create_dir("databases")
+    table_names = ['[MyModule].[tblTable1]']
+    module_name = 'MyModule'
+    database = create_simple_db_definition(db_scripts, module_name, table_names)
+    data_set = 'foo'
+
+    Dbt::Config.default_datasets_dir_name = 'dataset123'
+
+    mock.expects(:open).with(config, false).in_sequence(@s)
+    Dbt.runtime.expects(:info).with("Dumping [MyModule].[tblTable1]").in_sequence(@s)
+    mock.expects(:query).with("SELECT * FROM [MyModule].[tblTable1]").returns([{'ID' => 1}, {'ID' => 2}]).in_sequence(@s)
+    mock.expects(:close).with().in_sequence(@s)
+
+    Dbt.runtime.dump_database_to_fixtures(database, fixture_dir, :data_set => data_set)
+
+    assert_file_exist("#{fixture_dir}/MyModule/dataset123/foo/MyModule.tblTable1.yml")
+  end
+
   def test_dump_database_to_fixtures_with_filter
     mock = Dbt::DbDriver.new
     Dbt.runtime.instance_variable_set("@db", mock)
