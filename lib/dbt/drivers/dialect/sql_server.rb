5b4795ee382c5ca55ce3485abc061d83d9c41166
Peter Donald <peter@realityforge.org>
2015-06-23 14:03:32 +1000

    During drop operation, delete all associated history

--- lib/dbt/drivers/dialect/sql_server.rb
+++ lib/dbt/drivers/dialect/sql_server.rb
@@ -173,6 +173,7 @@ ORDER BY t.Ordinal, t.Name
 
       def drop(database, configuration)
         execute('SET DEADLOCK_PRIORITY HIGH')
+        execute("EXEC msdb.dbo.sp_delete_database_backuphistory @database_name = N'#{configuration.catalog_name}'")
         if configuration.force_drop?
           execute(<<SQL)
   IF EXISTS
