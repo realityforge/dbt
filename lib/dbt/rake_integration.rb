9876178580c7ccb7907d76c9378ec626e6e0e319
Peter Donald <peter@realityforge.org>
2015-10-28 09:25:54 +1100

    If an example file exists and no real configuration is present then copy the example across

--- lib/dbt/rake_integration.rb
+++ lib/dbt/rake_integration.rb
@@ -266,6 +266,20 @@ INSERT INTO @@TARGET@@.#{entity.sql.qualified_table_name}(#{entity.attributes.se
         end
       end
 
+      task Dbt::Config.config_filename do
+        target = Dbt::Config.config_filename
+        unless File.exist?(target)
+          target_ext = File.extname(target)
+          if '' != target_ext
+            source = "#{target[0, target.size-target_ext.size]}.example#{target_ext}"
+            if File.exist?(source)
+              @@runtime.info("Copying sample configuration file from #{source} to #{target}")
+              FileUtils.cp source, target
+            end
+          end
+        end
+      end
+
       task "#{Dbt::Config.task_prefix}:all:pre_build"
 
       @@defined_init_tasks = true
